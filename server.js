6a7a556a1366acd8c16421bd58ad723822b7dc39
Joaquin Florencio <Jrr232@gmail.com>
2025-09-07 16:34:53 -0400

    Fix enter page component

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // Serve static files from the Vite build
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handle client-side routing
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 // Listen on Heroku port
 const PORT = process.env.PORT || 3000;
