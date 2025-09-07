ff329b061b30260146c89acd3b247ded5523b0d5
Joaquin Florencio <Jrr232@gmail.com>
2025-09-07 15:57:54 -0400

    fixed package.jsono

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";  // <-- add this
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 // Serve static files from the Vite build
